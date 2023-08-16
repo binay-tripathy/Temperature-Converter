73013f590ad812b531f673b7f80eb2a814b1f72d
Binay Tripathy <binaytripathy123@gmail.com>
2023-08-16 19:32:05 +0530

    Some Changes

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,65 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import temppng from './temp.png'
 
 const Converter = () => {
+    const [temp, setTemp] = useState();
+
+    const tempInput = (event) =>{
+        setTemp(event.target.value);
+        
+    }
+    console.log(temp);
+    
     return (
-        <section className="vh-100">
-            <div className="container py-5 h-100">
 
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-md-8 col-lg-6 col-xl-5">
+        <div className="card">
+            <h5 className="card-header text-center">Temperature Converter</h5>
+            <div className="card-body">
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <input type="number" className="form-control" value={temp} onChange={tempInput} aria-label="Text input with dropdown button" />
+                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Select Temp.</button>
+                            <ul className="dropdown-menu dropdown-menu-end">
+                                <li className="dropdown-item">Celcius</li>
+                                <li className="dropdown-item">Fahrenheit</li>
+                                <li className="dropdown-item">Kelvin</li>
+                            </ul>
 
-                        <div className="card">
-                            <h5 className="card-header text-center">Temperature Converter</h5>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col">
-                                        <div className="input-group mb-3">
-                                            <input type="number" className="form-control" aria-label="Text input with dropdown button" />
-                                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Select Temp.</button>
-                                            <ul className="dropdown-menu dropdown-menu-end">
-                                                <li className="dropdown-item">Celcius</li>
-                                                <li className="dropdown-item">Fahrenheit</li>
-                                                <li className="dropdown-item">Kelvin</li>
-                                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col tempimg">
+                        <img src={temppng} alt="temp" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col">
 
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col tempimg">
-                                        <img src={temppng} alt="temp" />
-                                    </div>
-                                </div>
-                                <div className="row mb-3">
-                                    <div className="col">
+                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Select Temp.</button>
+                        <ul className="dropdown-menu dropdown-menu-end">
+                            <li className="dropdown-item">Celcius</li>
+                            <li className="dropdown-item">Fahrenheit</li>
+                            <li className="dropdown-item">Kelvin</li>
+                        </ul>
+                    </div>
+                    <div className="col">
 
-                                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Select Temp.</button>
-                                        <ul className="dropdown-menu dropdown-menu-end">
-                                            <li className="dropdown-item">Celcius</li>
-                                            <li className="dropdown-item">Fahrenheit</li>
-                                            <li className="dropdown-item">Kelvin</li>
-                                        </ul>
-                                    </div>
-                                    <div className="col">
-
-                                        <a href="/" className="btn btn-primary">Convert</a>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <div className="input-group mb-2">
-                                            <label htmlFor="disabled-input">Result: </label>
-                                            <input type="number" id='disabled-input' className="form-control mx-2 mb-2" aria-label="Disabled input with dropdown button" disabled readonly />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        <a href="/" className="btn btn-primary">Convert</a>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-2">
+                            <label htmlFor="disabled-input">Result: </label>
+                            <input type="number" id='disabled-input' className="form-control mx-2 mb-2" aria-label="Disabled input with dropdown button" disabled readOnly />
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,17 @@ import Converter from './Converter'
 const Layout = () => {
   return (
     <>
-    <Converter/>
+      <section className="vh-100">
+        <div className="container py-5 h-100">
+
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-md-8 col-lg-6 col-xl-5">
+
+              <Converter />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
 
   )
